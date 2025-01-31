fa3268dd2b4dba8d4feea33ca62f45fa9c83b8cc
mstrilec <MaxStrilec2005@gmail.com>
2025-01-31 15:50:51 +0200

    deploy links

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
-import Cookies from 'js-cookie';
+import Cookies from 'js-cookie'
 
 const api = axios.create({
-	baseURL: 'http://localhost:5000',
+	baseURL: 'https://kevych-task.onrender.com',
 })
 
 api.interceptors.request.use(config => {
