26c8aa2d49c3b5e99541da6cebc398383a9958c7
m.strilec <59706893+mstrilec@users.noreply.github.com>
2025-02-07 13:56:42 +0200

    Update cors options

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'https://kevych-task.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
