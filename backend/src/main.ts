3b6041d5c898cbdd885fc97c6ceb4610c1ce4433
m.strilec <59706893+mstrilec@users.noreply.github.com>
2025-02-07 13:51:25 +0200

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://kevych-task.vercel.app',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
   });
+  
+  console.log('CORS enabled');
+  
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
